e8ab822b78a5ad7227dfcc03339b84819622ce84
Jiaqi He <jh1547@scarletmail.rutgers.edu>
2024-02-18 18:07:35 -0500

    changed Player Controller script and add the animation for player

--- Assets/Scripts/CharacterControl/PlayerController.cs
+++ Assets/Scripts/CharacterControl/PlayerController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    private enum animState{ Idle, MoveForward, MoveLeft, MoveRight, MoveBackward, Sprint, Attack}
     private CharacterController _controller;
-    
+    private Animator anim;
+    private animState state;
     [SerializeField]
     private float movementSpeed = 5;
     
@@ -13,13 +15,14 @@ public class PlayerController : MonoBehaviour
     
     public Camera followCamera { get; set; }
 
-    //public bool attack;
+    public bool attack;
 
     //public GameObject enemy;
     
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
     
     void Update()
@@ -30,28 +33,35 @@ public class PlayerController : MonoBehaviour
         //{
         //    if (Input.GetKeyDown(KeyCode.F))
         //    {
-                
+
         //        enemy.GetComponent<HPScript>().hp -= 5;
         //    }
         //}
-        Movement();
 
+        Movement();
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            state = animState.Attack;
+        }
+        anim.SetInteger("state", (int)state);
     }
 
     void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
         Vector3 movementInput = Quaternion.Euler(0, followCamera.transform.eulerAngles.y, 0) * new Vector3(horizontalInput, 0, verticalInput);
         Vector3 movementDirection = movementInput.normalized;
-
+        state = animState.Idle;
+        if (movementInput != Vector3.zero) {
+         state = animState.MoveForward;
+        }
         if (movementDirection != Vector3.zero)
         {
             Quaternion desiredRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
         }
-
+        anim.SetInteger("state", (int)state);
         _controller.Move(movementDirection * movementSpeed * Time.deltaTime);
     }
 }
